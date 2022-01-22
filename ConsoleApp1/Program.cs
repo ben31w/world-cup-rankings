3b2fe0b562c82eb86b2724c030dad061df5f7521
Ben <Ben@DESKTOP-23BR7S9>
2022-01-21 18:21:08 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Program360
+{
+    public class Soccer
+    {
+        public static string[] getRankings(string[] matches)
+        {
+            // Your program begins here!
+            return null;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
